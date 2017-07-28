227384eea22506e0eee421899a132c8ee170680a
wwSchrader <wwschrader@gmail.com>
2017-07-28 00:47:22 -0700

    Add fields for new vote page

--- client/src/NewVotePage.js
+++ client/src/NewVotePage.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
-import { PageHeader } from 'react-bootstrap';
+import update from 'immutability-helper';
+import { PageHeader, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
 class NewVotePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            voteName: '',
+            voteOptions: ['', '']
+        };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(e) {
+        this.setState({ voteName: e.target.value });
+    }
+
+    handleOptionChange(index, e) {
+        let newState = update(
+                this.state,{
+                    voteOptions: {
+                        [index]: {$set: e.target.value}
+                    }
+                }
+            );
+        this.setState(newState);
+    }
+
   render() {
+    let voteOptions = this.state.voteOptions.map((option, index) => {
+        return (
+            <FormControl key={index}
+                type="text"
+                value={this.state.voteOptions[index]}
+                placeholder="Name of option"
+                onChange={this.handleOptionChange.bind(this, index)}
+            />
+        );
+    })
     return (
       <div className="Home">
         <PageHeader>Create a new Vote.</PageHeader>
         <p>Fill out the form below to create your vote!</p>
+
+          <form>
+            <FormGroup controlId="formVoteName">
+                <ControlLabel>Vote Name</ControlLabel>
+                <FormControl
+                    type="text"
+                    value={this.state.voteName}
+                    placeholder="Name of the Vote"
+                    onChange={this.handleNameChange}
+                />
+                <FormControl.Feedback />
+            </FormGroup>
+            <FormGroup controlId="formVoteOptions">
+                <ControlLabel>Vote Options</ControlLabel>
+                    {voteOptions}
+                    <FormControl.Feedback />
+            </FormGroup>
+          </form>
       </div>
     );
   }
