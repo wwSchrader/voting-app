63e446c98b7465c926034695291242a538bc89bc
wwSchrader <wwschrader@gmail.com>
2017-08-05 23:12:42 -0700

    Remove some console.logs

--- routes/addoption.js
+++ routes/addoption.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.put('/', function(req, res) {
-    console.log("add option");
     req.datastore.addOptionAndVote(req.query.id, req.query.votename)
         .then(response => {
-            console.log(response.result.ok);
             if (response.result.ok !== 1) {
                 res.sendStatus(500);
             } else {
