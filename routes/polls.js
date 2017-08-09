1e3febbbffd5c7425c96f5b0313800f5619399c4
wwSchrader <wwschrader@gmail.com>
2017-08-09 16:38:17 -0700

    Add id tracking for poll creators and voters to the poll objects in the database

--- routes/polls.js
+++ routes/polls.js
@@ -5,6 +5,7 @@ var { ensureAuthenticated } = require('../library.js');
 
 router.post('/', ensureAuthenticated, function(req, res) {
     let newPoll = Object.assign({}, req.body);
+    console.log(req.user);
 
     //convert option name array into an array of objects with name and vote attributes
     newPoll.voteOptions = req.body.voteOptions.map((option) => {
@@ -13,6 +14,10 @@ router.post('/', ensureAuthenticated, function(req, res) {
             optionVotes: 0
         });
     });
+
+    //add fields to keep track of poll creator and voters
+    newPoll.creatorId = req.user._id;
+    newPoll.voterList = [];
 
     req.datastore.insert(newPoll)
         .then(response => {
